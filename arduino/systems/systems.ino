bb122772bfbc9dae1e4f0935b595136c18033fd4
mouad <mouadsaif.ms@gmail.com>
2023-02-28 01:28:00 +0100

    if leak then block water

--- arduino/systems/systems.ino
+++ arduino/systems/systems.ino
@@ -7,7 +7,7 @@ int data = 0;
 const int buzzer = 9; // attach buzzer to pin 9
 const int waterSensor = A0; // attach sensor to pin a0
 int sensorValue;
-
+bool isleak = 0;
 char buffer[40];
 
 
@@ -39,7 +39,7 @@ void loop() {
   distance = t / 57;
   delay(50);
 
-  if (distance < 15 && distance > 5  ) {
+  if (distance < 15 && distance > 5 && isleak == 0 ) {
     tempConsomation1 = millis();
 
     digitalWrite(waterPump, HIGH);
@@ -59,7 +59,7 @@ void loop() {
     tone(buzzer, 800, 500);
     delay(200);
     tone(buzzer, 600, 500);
-
+    isleak = 1;
     sprintf(buffer, "1.%d", tempConsomation/1000);
     Serial.println(buffer);
     delay(500);
@@ -68,6 +68,7 @@ void loop() {
   else {
     sprintf(buffer, "0.%d", tempConsomation / 1000);
     Serial.println(buffer);
+     isleak = 0;
     delay(500);
   }
 
@@ -76,12 +77,12 @@ void loop() {
 
   if (Serial.available() > 0) {
     data = Serial.read();
-    if (data == '1') {
+    if (data == '1' && isleak == 0 ) {
       digitalWrite(waterPump, HIGH);
       delay(2000);
       digitalWrite(waterPump, LOW);
       tempConsomation = tempConsomation +1000;
-    } else if (data == '0') {
+    } else if (data == '0' && !isleak) {
       digitalWrite(waterPump, HIGH);
       delay(4000);
       digitalWrite(waterPump, LOW);
